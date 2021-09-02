a98bdf80d899a8dd57213f0e89125e24097c1c5f
garfield <71535863+2dgeneralspam1@users.noreply.github.com>
2021-09-02 17:22:19 -0400

    Dhar Man Title Generator
    makes random sentences that somewhat sound like dhar man titles

---
+++ dhar-man/title-generator.py
@@ -0,0 +1,112 @@
+while True: 
+    import random
+
+    wordsOne = [
+        "Kid",
+        "Adult",
+        "Man",
+        "Bully",
+        "Felon",
+        "Teacher",
+        "Karen",
+        "Woman",
+        "Boy",
+        "Friend",
+        "President",
+        "Gold Digger",
+        "Best Friend",
+        "Fake Friend",
+        "Nerd",
+        "Tik Toker",
+        "Rich person",
+        "Poor person",
+        "Fake instagrammer",
+        "Joe Biden",
+        "Donald Trump",
+        "Judge",
+        "Lawyer"
+    ]
+
+    wordsTwo = [
+        "Kills",
+        "Beats",
+        "Makes Fun Of",
+        "Punches",
+        "Shoots",
+        "Tortures",
+        "Convicts",
+        "Steals",
+        "Robs",
+        "Destroys",
+        "Burns",
+        "Rips apart",
+        "Burns alive",
+        "Kicks",
+        "Locks in basement",
+        "Tears apart",
+        "Farts on",
+        "Throws off roof",
+        "Eats",
+        "Fat shames",
+        "Bullies",
+        "Judges"
+    ]
+
+    wordsThree = [
+        "Kid",
+        "Adult",
+        "Man",
+        "Bully",
+        "Felon",
+        "Teacher",
+        "Karen",
+        "Woman",
+        "Boy",
+        "Friend",
+        "President",
+        "Car",
+        "House",
+        "Phone",
+        "Purse",
+        "Wallet",
+        "Tank",
+        "Pizza",
+        "Chicken",
+        "Poor person",
+        "Rich person",
+        "Dumb kid",
+        "Kid with ADHD",
+        "Fat kid",
+        "Skinny kid",
+        "Poor kid",
+        "Rich kid",
+        "Female",
+        "Joe Biden",
+        "Donald Trump",
+        "Taxes"
+    ]
+
+    wordsFour = [ 
+        "Instantly regrets it",
+        "What happens next is shocking",
+        "Lives to regret it",
+        "Gets convicted",
+        "Gets sent to jail",
+        "Gets burned by Donald Trump",
+        "Gets sent to jail for 500 years",
+        "Instantly dies",
+        "Gets killed by karma instantly",
+        "Falls off a cliff",
+        "Gets run over by a tractor",
+        "Gets awarded",
+        "Gets a prize",
+        "Gets the Noble Peace Prize",
+        "Buys a chicken",
+        "Dies",
+        "Dies, but then comes back alive then dies again",
+        "Gets burned alive",
+        "Dies from a toaster"
+    ]
+
+
+    input(f"{random.choice(wordsOne)} {random.choice(wordsTwo)} {random.choice(wordsThree)}, {random.choice(wordsFour)}!")
